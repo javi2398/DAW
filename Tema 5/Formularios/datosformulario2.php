44179bdebfd4f20e92fb012df5db9f166c7dbb96
javi2398 <69079578+javi2398@users.noreply.github.com>
2024-09-17 08:44:28 +0200

    Add files via upload

---
+++ Tema 5/Formularios/datosformulario2.php
@@ -0,0 +1,20 @@
+<?php
+$nombre=$_POST['nombre'];
+echo $nombre
+
+$email=$_POST['email'];
+echo $email
+
+$telefono=$_POST['telefono'];
+echo $telefono
+
+$sitioweb=$_POST['sitioweb'];
+echo $sitioweb
+
+$asunto=$_POST['asunto'];
+echo $asunto
+
+$mensaje=$_POST['mensaje'];
+echo $mensaje
+
+?>
